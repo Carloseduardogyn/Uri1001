1a8bdd983c68a0486b7e4e44b1495548f28565e8
unknown <carloseduardogomeslima1@gmail.com>
2023-01-26 09:41:45 -0300

    Soma de dois inteiros

---
+++ Uri1001.java
@@ -0,0 +1,28 @@
+package Uri;
+
+import java.util.Scanner;
+
+public class Uri1001 {
+
+	public static void main(String[] args) {
+	
+        Scanner sc = new Scanner(System.in);
+		
+		int a, b, X;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		X =  a + b;
+		
+		System.out.println("X = " + X);
+		
+		
+        sc.close();
+	
+
+
+		
+	}
+
+}
